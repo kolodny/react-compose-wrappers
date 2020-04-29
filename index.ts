559f1a148b4499f702106df330ab6b0b57fe39bb
Moshe Kolodny <kolodny@github.com>
2020-04-29 13:46:57 -0400

    No need to spread operator

--- index.ts
+++ index.ts
@@ -4,6 +4,10 @@ export const composeWrappers = (
   wrappers: React.FunctionComponent[]
 ): React.FunctionComponent => {
   return wrappers.reduce((Acc, Current): React.FunctionComponent => {
-    return props => <Current><Acc {...props} /></Current>
+    return props => React.createElement(
+      Current,
+      null,
+      React.createElement(Acc, props as any)
+    );
   });
 }
