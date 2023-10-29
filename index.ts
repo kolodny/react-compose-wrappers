50ca9ecd82675143fbfac28e65643e5ace8155e6
Moshe Kolodny <mkolodny@netflix.com>
2023-10-29 12:12:16 -0400

    Type safe with new React types

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
 import React from 'react';
 
-type Wrapper = React.FC<React.PropsWithChildren<{}>>;
+type Wrapper = React.FunctionComponent<{ children: React.ReactNode }>;
 
-export const composeWrappers = (
-  wrappers: Wrapper[]
-): Wrapper => {
+export const composeWrappers = (wrappers: Wrapper[]): Wrapper => {
   return wrappers.reduceRight((Acc, Current): Wrapper => {
-    return props => React.createElement(
-      Current,
-      null,
-      React.createElement(Acc, props as any)
-    );
+    return (props) =>
+      React.createElement(
+        Current,
+        null,
+        React.createElement(Acc, props as any)
+      );
   });
-}
+};
